9f7915bad9ac898b81ca1f74f461c9beb2957ac3
kevin <kevin.truman@fusi24.com>
2018-08-26 09:37:26 +0700

    first commit

---
+++ src/main/java/com/dev/kev/KevApplication.java
@@ -0,0 +1,12 @@
+package com.dev.kev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevApplication.class, args);
+	}
+}
